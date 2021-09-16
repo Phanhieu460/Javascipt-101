5134fb478c474d1f786881b21543846f71879d99
phanhieu1312 <phanbahieu460@gmail.com>
2021-09-16 23:02:02 +0700

    Update TodoList remove

--- ToDo/index.js
+++ ToDo/index.js
@@ -7,10 +7,41 @@ function addTodo(event) {
 
   const todoDiv = document.createElement("div");
   todoDiv.classList.add("todo");
+
   const todoLi = document.createElement("li");
-  todoLi.innerHTML = "hey";
+  todoLi.innerHTML = todoInput.value;
   todoLi.classList.add("todo-item");
   todoDiv.appendChild(todoLi);
+
+  const completeButton = document.createElement("button");
+  completeButton.innerHTML = '<i class="fas fa-check"></i>';
+  completeButton.classList.add("complete-btn");
+  todoDiv.appendChild(completeButton);
+
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  todoList.appendChild(todoDiv);
+  todoInput.value = "";
+}
+function deleteTodo(e) {
+  e.preventDefault();
+  const item = e.target;
+  if (item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener("transitionend", () => {
+      todo.remove();
+    });
+  }
+  if (item.classList[0] === "complete-btn") {
+    const todo = item.parentElement;
+    todo.classList.toggle("completed");
+  }
 }
 
 todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
